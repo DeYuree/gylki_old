d96cbdad1074bd6fbc938550912afbfa49fd4f00
DeYuree <complexshket@gmail.com>
2021-10-19 18:21:21 -0400

    changes from 19.10.2021 with gylki routes

---
+++ _nuxt/static/1634681247/catalog/transport/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/catalog/transport", {data:[{},{}],fetch:{},mutations:[["products\u002FSET_CURRENT_CATEGORY",{_id:"601e7e2edee00da5e64d3d6f",active:true,link:"transport",name:"Транспорт",icon:"fas fa-biking"}],["products\u002FRESET_CURRENT_PAGE",void 0],["products\u002FSET_PRODUCTS_FOR_CATEGORY",[]]]});
